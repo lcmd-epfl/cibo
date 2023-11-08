Fix normalization bug and update experiment configurations

1_greedy_fix_costs/distance/costs_min.py
@@ -10,41 +10,59 @@ from utils import *
 RESULTS = []
 
 for exp_config in benchmark:
-    DATASET = Evaluation_data(exp_config["dataset"], exp_config["ntrain"], "random", init_strategy=exp_config["init_strategy"])
+    DATASET = Evaluation_data(
+        exp_config["dataset"],
+        exp_config["ntrain"],
+        "random",
+        init_strategy=exp_config["init_strategy"],
+    )
     bounds_norm = DATASET.bounds_norm
 
-    N_RUNS            = exp_config["n_runs"]
-    NITER             = exp_config["n_iter"]
-    BATCH_SIZE        = exp_config["batch_size"]
-    MAX_BATCH_COST    = exp_config["max_batch_cost"]
-    COST_AWARE_BO     = exp_config["cost_aware"]
+    N_RUNS = exp_config["n_runs"]
+    NITER = exp_config["n_iter"]
+    BATCH_SIZE = exp_config["batch_size"]
+    MAX_BATCH_COST = exp_config["max_batch_cost"]
+    COST_AWARE_BO = exp_config["cost_aware"]
     COST_AWARE_RANDOM = False
 
     y_better_BO_ALL, y_better_RANDOM_ALL = [], []
     running_costs_BO_ALL, running_costs_RANDOM_ALL = [], []
 
     for run in range(N_RUNS):
-        SEED = 111+run
+        SEED = 111 + run
         random.seed(SEED)
         np.random.seed(SEED)
         torch.manual_seed(SEED)
 
-        X_init, y_init, costs_init, X_candidate, y_candidate, costs_candidate = DATASET.get_init_holdout_data(SEED)
+        (
+            X_init,
+            y_init,
+            costs_init,
+            X_candidate,
+            y_candidate,
+            costs_candidate,
+        ) = DATASET.get_init_holdout_data(SEED)
         X, y = cp.deepcopy(X_init), cp.deepcopy(y_init)
         y_best = float(torch.max(y))
         model, scaler_y = update_model(X, y, bounds_norm)
 
-        X_candidate_FULL, y_candidate_FULL = cp.deepcopy(X_candidate), cp.deepcopy(y_candidate)
-        costs_FULL          = cp.deepcopy(costs_candidate)
-        X_candidate_BO      = cp.deepcopy(X_candidate)
-        y_candidate_BO      = cp.deepcopy(y_candidate)
-        y_candidate_RANDOM  = cp.deepcopy(y_candidate).detach().numpy()
+        X_candidate_FULL, y_candidate_FULL = cp.deepcopy(X_candidate), cp.deepcopy(
+            y_candidate
+        )
+
+        pred = model.posterior(X_candidate_FULL).mean.detach().flatten().numpy()
+        validate_model = False
+
+        costs_FULL = cp.deepcopy(costs_candidate)
+        X_candidate_BO = cp.deepcopy(X_candidate)
+        y_candidate_BO = cp.deepcopy(y_candidate)
+        y_candidate_RANDOM = cp.deepcopy(y_candidate).detach().numpy()
 
         running_costs_BO = [0]
         running_costs_RANDOM = [0]
 
-        costs_BO        = cp.deepcopy(costs_candidate)
-        costs_RANDOM    = cp.deepcopy(costs_candidate)
+        costs_BO = cp.deepcopy(costs_candidate)
+        costs_RANDOM = cp.deepcopy(costs_candidate)
 
         y_better_BO = []
         y_better_RANDOM = []
@@ -53,64 +71,99 @@ for exp_config in benchmark:
         y_better_RANDOM.append(y_best)
         y_best_BO, y_best_RANDOM = y_best, y_best
 
+        BO_data = {
+            "model": model,
+            "y_best_BO": y_best_BO,
+            "scaler_y": scaler_y,
+            "X": X,
+            "y": y,
+            "N_train": len(X),
+            "X_candidate_BO": X_candidate_BO,
+            "y_candidate_BO": y_candidate_BO,
+            "y_better_BO": y_better_BO,
+            "costs_BO": costs_BO,
+            "running_costs_BO": running_costs_BO,
+            "bounds_norm": bounds_norm,
+            "BATCH_SIZE": BATCH_SIZE,
+            "MAX_BATCH_COST": MAX_BATCH_COST,
+        }
 
-        BO_data = {"model":model, 
-                   "y_best_BO": y_best_BO,
-                   "scaler_y":scaler_y, 
-                   "X":X, 
-                   "y":y, 
-                   "N_train":len(X),
-                   "X_candidate_BO":X_candidate_BO, 
-                   "y_candidate_BO":y_candidate_BO, 
-                   "y_better_BO": y_better_BO,
-                   "costs_BO":costs_BO, 
-                   "running_costs_BO":running_costs_BO,
-                   "bounds_norm":bounds_norm,
-                   "BATCH_SIZE":BATCH_SIZE,
-                   "MAX_BATCH_COST":MAX_BATCH_COST}
-        
-        RANDOM_data_ = {"y_candidate_RANDOM":y_candidate_RANDOM,
-                        "y_best_RANDOM":y_best_RANDOM,
-                        "costs_RANDOM":costs_RANDOM,
-                        "BATCH_SIZE":BATCH_SIZE,
-                        "MAX_BATCH_COST":MAX_BATCH_COST,
-                        "y_better_RANDOM":y_better_RANDOM,
-                        "running_costs_RANDOM":running_costs_RANDOM,
-                        }
+        RANDOM_data_ = {
+            "y_candidate_RANDOM": y_candidate_RANDOM,
+            "y_best_RANDOM": y_best_RANDOM,
+            "costs_RANDOM": costs_RANDOM,
+            "BATCH_SIZE": BATCH_SIZE,
+            "MAX_BATCH_COST": MAX_BATCH_COST,
+            "y_better_RANDOM": y_better_RANDOM,
+            "running_costs_RANDOM": running_costs_RANDOM,
+        }
 
         for i in range(NITER):
             if COST_AWARE_BO == False:
                 BO_data = BO_CASE_1_STEP(BO_data)
-            else:    
-                SUCCESS = False
-                indices, candidates = gibbon_search(model, X_candidate_BO,bounds_norm, q=BATCH_SIZE)
-                suggested_costs = costs_BO[indices].flatten()
-                cheap_indices   = select_batch(suggested_costs, MAX_BATCH_COST, BATCH_SIZE)
-                cheap_indices, SUCCESS_1         = check_success(cheap_indices, indices)
-                ITERATION = 1
-                
 
-                while (cheap_indices ==[]) or (len(cheap_indices) < BATCH_SIZE):
+                if validate_model:
+                    pred = (
+                        scaler_y.inverse_transform(
+                            BO_data["model"]
+                            .posterior(BO_data["X_candidate_BO"])
+                            .mean.detach()
+                        )
+                        .flatten()
+                        .numpy()
+                    )
+                    # var = np.sqrt(model.posterior(X_candidate_FULL).variance.detach().flatten().numpy())
+                    # scaler_y.inverse_transform(model.posterior(X_candidate_FULL).mean.detach()).flatten().numpy()
+                    y_test = BO_data[
+                        "y_candidate_BO"
+                    ].flatten()  # y_candidate_FULL.numpy().flatten()
+                    plt.clf()
+                    plt.close()
+                    plt.scatter(pred, y_test, marker="o")
+                    plt.savefig("shit.png")
+            else:
+                SUCCESS = False
+                indices, candidates = gibbon_search(
+                    model, X_candidate_BO, bounds_norm, q=BATCH_SIZE
+                )
+                suggested_costs = costs_BO[indices].flatten()
+                cheap_indices = select_batch(
+                    suggested_costs, MAX_BATCH_COST, BATCH_SIZE
+                )
+                cheap_indices, SUCCESS_1 = check_success(cheap_indices, indices)
+                ITERATION = 1
+
+                while (cheap_indices == []) or (len(cheap_indices) < BATCH_SIZE):
                     INCREMENTED_MAX_BATCH_COST = MAX_BATCH_COST
                     SUCCESS_1 = False
 
                     INCREMENTED_BATCH_SIZE = BATCH_SIZE + ITERATION
-                    print("Incrementing canditates for batch to: ", INCREMENTED_BATCH_SIZE)
+                    print(
+                        "Incrementing canditates for batch to: ", INCREMENTED_BATCH_SIZE
+                    )
                     if INCREMENTED_BATCH_SIZE > len(X_candidate_BO):
                         print("Not enough candidates left to account for the costs")
-                        #therefore increasing the max batch cost to finally get enough candidates
-                        INCREMENTED_MAX_BATCH_COST  += 1
+                        # therefore increasing the max batch cost to finally get enough candidates
+                        INCREMENTED_MAX_BATCH_COST += 1
 
-                    indices, candidates = gibbon_search(model, X_candidate_BO,bounds_norm, q=INCREMENTED_BATCH_SIZE)
+                    indices, candidates = gibbon_search(
+                        model, X_candidate_BO, bounds_norm, q=INCREMENTED_BATCH_SIZE
+                    )
                     suggested_costs = costs_BO[indices].flatten()
 
+                    cheap_indices = select_batch(
+                        suggested_costs, INCREMENTED_MAX_BATCH_COST, BATCH_SIZE
+                    )
+                    cheap_indices, SUCCESS_2 = check_success(cheap_indices, indices)
 
-                    cheap_indices   = select_batch(suggested_costs, INCREMENTED_MAX_BATCH_COST, BATCH_SIZE)
-                    cheap_indices, SUCCESS_2         = check_success(cheap_indices, indices)
-                    
-
-                    if (cheap_indices !=[]) and len(cheap_indices) == BATCH_SIZE:
-                        X, y = update_X_y(X, y, X_candidate_BO[cheap_indices], y_candidate_BO, cheap_indices)
+                    if (cheap_indices != []) and len(cheap_indices) == BATCH_SIZE:
+                        X, y = update_X_y(
+                            X,
+                            y,
+                            X_candidate_BO[cheap_indices],
+                            y_candidate_BO,
+                            cheap_indices,
+                        )
                         y_best_BO = check_better(y, y_best_BO)
 
                         y_better_BO.append(y_best_BO)
@@ -118,72 +171,121 @@ for exp_config in benchmark:
                         print("Batch cost1: ", BATCH_COST)
                         running_costs_BO.append(running_costs_BO[-1] + BATCH_COST)
                         model, scaler_y = update_model(X, y, bounds_norm)
-                    
 
-                        X_candidate_BO = np.delete(X_candidate_BO, cheap_indices, axis=0)
-                        y_candidate_BO = np.delete(y_candidate_BO, cheap_indices, axis=0)
+                        X_candidate_BO = np.delete(
+                            X_candidate_BO, cheap_indices, axis=0
+                        )
+                        y_candidate_BO = np.delete(
+                            y_candidate_BO, cheap_indices, axis=0
+                        )
                         costs_BO = np.delete(costs_BO, cheap_indices, axis=0)
                         break
-                    
-                    ITERATION +=1
+
+                    ITERATION += 1
 
                 if SUCCESS_1:
-                    X, y = update_X_y(X, y, X_candidate_BO[cheap_indices], y_candidate_BO, cheap_indices)
+                    X, y = update_X_y(
+                        X,
+                        y,
+                        X_candidate_BO[cheap_indices],
+                        y_candidate_BO,
+                        cheap_indices,
+                    )
                     y_best_BO = check_better(y, y_best_BO)
                     y_better_BO.append(y_best_BO)
                     BATCH_COST = sum(costs_BO[cheap_indices])[0]
                     print("Batch cost2: ", BATCH_COST)
                     running_costs_BO.append(running_costs_BO[-1] + BATCH_COST)
-                    model,scaler_y = update_model(X, y, bounds_norm)
+                    model, scaler_y = update_model(X, y, bounds_norm)
                     X_candidate_BO = np.delete(X_candidate_BO, cheap_indices, axis=0)
                     y_candidate_BO = np.delete(y_candidate_BO, cheap_indices, axis=0)
                     costs_BO = np.delete(costs_BO, cheap_indices, axis=0)
 
             if COST_AWARE_RANDOM == False:
-                indices_random = np.random.choice(np.arange(len(y_candidate_RANDOM)), size=BATCH_SIZE, replace=False)
-                if max(y_candidate_RANDOM[indices_random])[0] > y_best_RANDOM:
-                    y_best_RANDOM = max(y_candidate_RANDOM[indices_random])[0] 
-
-                y_better_RANDOM.append(y_best_RANDOM)
-                BATCH_COST = sum(costs_RANDOM[indices_random])[0]
-                running_costs_RANDOM.append(running_costs_RANDOM[-1] + BATCH_COST)
-                y_candidate_RANDOM = np.delete(y_candidate_RANDOM, indices_random, axis=0)
-                costs_RANDOM = np.delete(costs_RANDOM, indices_random, axis=0)
-            else:
-                all_cheapest_indices = np.argwhere(costs_RANDOM.flatten() == 0).flatten()
-                indices_random = np.random.choice(all_cheapest_indices, size=BATCH_SIZE, replace=False)
+                indices_random = np.random.choice(
+                    np.arange(len(y_candidate_RANDOM)), size=BATCH_SIZE, replace=False
+                )
                 if max(y_candidate_RANDOM[indices_random])[0] > y_best_RANDOM:
                     y_best_RANDOM = max(y_candidate_RANDOM[indices_random])[0]
 
                 y_better_RANDOM.append(y_best_RANDOM)
                 BATCH_COST = sum(costs_RANDOM[indices_random])[0]
                 running_costs_RANDOM.append(running_costs_RANDOM[-1] + BATCH_COST)
-                y_candidate_RANDOM = np.delete(y_candidate_RANDOM, indices_random, axis=0)
+                y_candidate_RANDOM = np.delete(
+                    y_candidate_RANDOM, indices_random, axis=0
+                )
+                costs_RANDOM = np.delete(costs_RANDOM, indices_random, axis=0)
+            else:
+                all_cheapest_indices = np.argwhere(
+                    costs_RANDOM.flatten() == 0
+                ).flatten()
+                indices_random = np.random.choice(
+                    all_cheapest_indices, size=BATCH_SIZE, replace=False
+                )
+                if max(y_candidate_RANDOM[indices_random])[0] > y_best_RANDOM:
+                    y_best_RANDOM = max(y_candidate_RANDOM[indices_random])[0]
+
+                y_better_RANDOM.append(y_best_RANDOM)
+                BATCH_COST = sum(costs_RANDOM[indices_random])[0]
+                running_costs_RANDOM.append(running_costs_RANDOM[-1] + BATCH_COST)
+                y_candidate_RANDOM = np.delete(
+                    y_candidate_RANDOM, indices_random, axis=0
+                )
                 costs_RANDOM = np.delete(costs_RANDOM, indices_random, axis=0)
 
             print("--------------------")
-            print("# |{}/{}|\tBO {:.2f}\tRS {:.2f}\tSUM(COSTS BO): ${}\tSUM(COSTS RS): ${}\tN_train {}".format(i+1,
-                  NITER, BO_data["y_best_BO"], y_best_RANDOM, BO_data["running_costs_BO"][-1], running_costs_RANDOM[-1],BO_data["N_train"]))
+            print(
+                "# |{}/{}|\tBO {:.2f}\tRS {:.2f}\tSUM(COSTS BO): ${}\tSUM(COSTS RS): ${}\tN_train {}".format(
+                    i + 1,
+                    NITER,
+                    BO_data["y_best_BO"],
+                    y_best_RANDOM,
+                    BO_data["running_costs_BO"][-1],
+                    running_costs_RANDOM[-1],
+                    BO_data["N_train"],
+                )
+            )
 
         y_better_BO_ALL.append(y_better_BO)
         y_better_RANDOM_ALL.append(y_better_RANDOM)
         running_costs_BO_ALL.append(running_costs_BO)
         running_costs_RANDOM_ALL.append(running_costs_RANDOM)
 
-
     y_better_BO_ALL = np.array(y_better_BO_ALL)
     y_better_RANDOM_ALL = np.array(y_better_RANDOM_ALL)
 
     max_n = reaching_max_n(y_better_BO_ALL)
-    plot_utility_BO_vs_RS(y_better_BO_ALL, y_better_RANDOM_ALL, name="./figures/utility_{}_{}.png".format(exp_config["dataset"], exp_config["max_batch_cost"]))
-    plot_costs_BO_vs_RS(running_costs_BO_ALL, running_costs_RANDOM_ALL, name="./figures/optimization_{}_{}.png".format(exp_config["dataset"], exp_config["max_batch_cost"]))
+    plot_utility_BO_vs_RS(
+        y_better_BO_ALL,
+        y_better_RANDOM_ALL,
+        name="./figures/utility_{}_{}.png".format(
+            exp_config["dataset"], exp_config["max_batch_cost"]
+        ),
+    )
+    plot_costs_BO_vs_RS(
+        running_costs_BO_ALL,
+        running_costs_RANDOM_ALL,
+        name="./figures/optimization_{}_{}.png".format(
+            exp_config["dataset"], exp_config["max_batch_cost"]
+        ),
+    )
 
-    RESULTS.append({"settings": exp_config,"max_n":max_n ,"y_better_BO_ALL": y_better_BO_ALL, 
-                    "y_better_RANDOM_ALL": y_better_RANDOM_ALL, "running_costs_BO_ALL": running_costs_BO_ALL, 
-                    "running_costs_RANDOM_ALL": running_costs_RANDOM_ALL})
-    
+    RESULTS.append(
+        {
+            "settings": exp_config,
+            "max_n": max_n,
+            "y_better_BO_ALL": y_better_BO_ALL,
+            "y_better_RANDOM_ALL": y_better_RANDOM_ALL,
+            "running_costs_BO_ALL": running_costs_BO_ALL,
+            "running_costs_RANDOM_ALL": running_costs_RANDOM_ALL,
+        }
+    )
+
     print("Done with experiment: ", exp_config)
 
 print("Done with all experiments")
 print("Saving results")
-save_pkl(RESULTS, "results.pkl", )
+save_pkl(
+    RESULTS,
+    "results.pkl",
+)

BO.py
@@ -233,7 +233,7 @@ class CustomGPModel:
             if type(X_train) == np.ndarray:
                 X_train = torch.tensor(X_train, dtype=torch.float32)
 
-            X_train = normalize(X_train, bounds=self.bounds_norm).to(dtype=torch.float32) 
+            #X_train = normalize(X_train, bounds=self.bounds_norm).to(dtype=torch.float32) 
     
         y_train = self.scaler_y.fit_transform(y_train)
         