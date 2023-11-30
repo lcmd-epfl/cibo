90d4aad8d457a428f75313da82c03b437dab4da9
janweinreich <janweinreich286@gmail.com>
2023-11-30 14:16:42 +0100

    Add experiment configuration for BMS dataset with cost awareness

--- 2_scan_opt_update_costs_saving/increasing/exp_configs_2.py
+++ 2_scan_opt_update_costs_saving/increasing/exp_configs_2.py
@@ -13,6 +13,17 @@ benchmark = [
     {
         "dataset": "BMS",
         "init_strategy": "worst_ligand",
+        "cost_aware": True,
+        "n_runs": 5,
+        "n_iter": 15,
+        "batch_size": 5,
+        "max_batch_cost": 30.0,
+        "ntrain": 200,
+        "prices": "update_ligand_when_used",
+    },
+    {
+        "dataset": "BMS",
+        "init_strategy": "worst_ligand",
         "cost_aware": False,
         "n_runs": 5,
         "n_iter": 15,
