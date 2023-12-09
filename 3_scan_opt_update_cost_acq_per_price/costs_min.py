Fix price normalization bug and update gibbon search algorithm

3_scan_opt_update_cost_acq_per_price/costs_min.py
@@ -4,9 +4,8 @@ import random
 import copy as cp
 from exp_configs_2 import benchmark
 from BO import update_model
-from utils import Evaluation_data, create_aligned_transposed_price_table, create_data_dict_BO_2A, create_data_dict_RS_2A
-from experiments import Budget_schedule, plot_utility_BO_vs_RS, plot_costs_BO_vs_RS, save_pkl, BO_CASE_2A_STEP, RS_STEP_2A, BO_AWARE_SCAN_FAST_CASE_2_STEP_ACQ_PRICE
-import pdb
+from utils import Evaluation_data,Budget_schedule, plot_utility_BO_vs_RS, plot_costs_BO_vs_RS, save_pkl, create_aligned_transposed_price_table, create_data_dict_BO_2A, create_data_dict_RS_2A
+from experiments import BO_CASE_2A_STEP, RS_STEP_2A, BO_AWARE_SCAN_FAST_CASE_2_STEP_ACQ_PRICE
 
 SEED = 111
 random.seed(SEED)
@@ -59,7 +58,7 @@ if __name__ == "__main__":
             #X_init, y_init, costs_init, LIGANDS_init = np.array(X_init), np.array(y_init), np.array(costs_init), np.array(LIGANDS_init)
             #sort_idx = np.argsort(y_init, axis=0)
             #X_init, y_init, costs_init, LIGANDS_init = X_init[sort_idx], y_init[sort_idx], costs_init[sort_idx], LIGANDS_init[sort_idx]
-            
+            #pdb.set_trace()
             print(create_aligned_transposed_price_table(price_dict))
             X, y = cp.deepcopy(X_init), cp.deepcopy(y_init)
             y_best = float(torch.max(y))

BO.py
@@ -17,6 +17,10 @@ from torch.optim import Adam
 # Custom module imports
 from botroch_ext import optimize_acqf_discrete_modified
 from kernels import BoundedKernel, TanimotoKernel
+
+
+#specific import for the modified GIBBON function
+from utils import compute_price_acquisition_ligands_price_per_acqfct
 import pdb
 # Suppress warnings
 warnings.filterwarnings("ignore")
@@ -334,22 +338,24 @@ def gibbon_search_modified_all(
     return index_set,acq_values, candidates
 
 
-def is_descending(arr):
-    for i in range(len(arr) - 1):
-        if arr[i] < arr[i + 1]:
-            return False
-    return True
-
 def gibbon_search_modified_all_per_price(model, X_candidate_BO, bounds_norm, q, LIGANDS_candidate_BO,price_dict_BO):
-    from utils import compute_price_acquisition_ligands_price_per_acqfct
+    import matplotlib.pyplot as plt
 
     index_set,acq_values, candidates = gibbon_search_modified_all(model, X_candidate_BO, bounds_norm, q, sequential=False, maximize=True, n_best=300)
+    
     row_sums_1 = acq_values.sum(axis=1)
-    mean_row_sums_1 = 0.5*row_sums_1.mean()
+    #plt.hist(row_sums_1)
+    #plot the average as a line
+    #plt.axvline(x=row_sums_1.mean(), color='r', linestyle='dashed', linewidth=2)
+    #plt.savefig("row_sums_1.png")
+
+    
+    #mean_row_sums_1 = row_sums_1.mean() #
+    #mean_row_sums_1 = 0.1*mean_row_sums_1
     #find all indices where rows sum is larger than mean
-    index_set = index_set[row_sums_1 > mean_row_sums_1]
-    acq_values = acq_values[row_sums_1 > mean_row_sums_1]
-    candidates = candidates[row_sums_1 > mean_row_sums_1]
+    #index_set = index_set[row_sums_1 > mean_row_sums_1]
+    #acq_values = acq_values[row_sums_1 > mean_row_sums_1]
+    #candidates = candidates[row_sums_1 > mean_row_sums_1]
 
 
     ligand_set = []

experiments.py
@@ -1,12 +1,31 @@
-from BO import *
-from utils import *
+import numpy as np
+import torch
 
-SEED = 111
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
+from BO import (
+    update_model, 
+    gibbon_search, 
+    gibbon_search_modified, 
+    gibbon_search_modified_all, 
+    gibbon_search_modified_all_per_price
+)
+
+from utils import (
+    check_better, 
+    update_X_y, 
+    compute_price_acquisition_ligands, 
+    update_price_dict_ligands, 
+    select_batch, 
+    check_success, 
+    find_optimal_batch
+)
+
 
 def RS_STEP(RANDOM_data):
+    """
+    Simple RS step without any cost constraints but keep track of the costs
+    assuming random costs for each point and no cost updates
+    """
+
     # Extract the data from the dictionary
     y_candidate_RANDOM = RANDOM_data["y_candidate_RANDOM"]
     y_best_RANDOM = RANDOM_data["y_best_RANDOM"]
@@ -901,7 +920,6 @@ def BO_AWARE_SCAN_FAST_CASE_2_STEP_ACQ_PRICE(BO_data):
     # Get current BO data from last iteration
     model = BO_data["model"]
     X, y = BO_data["X"], BO_data["y"]
-    N_train = BO_data["N_train"]
     y_candidate_BO = BO_data["y_candidate_BO"]
     X_candidate_BO = BO_data["X_candidate_BO"]
     bounds_norm = BO_data["bounds_norm"]
@@ -914,15 +932,13 @@ def BO_AWARE_SCAN_FAST_CASE_2_STEP_ACQ_PRICE(BO_data):
     scaler_y = BO_data["scaler_y"]
     
     
-    index_set_rearranged, acq_values_per_price, candidates_rearranged  = gibbon_search_modified_all_per_price(model, X_candidate_BO, bounds_norm, q=BATCH_SIZE, LIGANDS_candidate_BO=LIGANDS_candidate_BO, price_dict_BO=price_dict_BO)
+    index_set_rearranged, _, candidates_rearranged  = gibbon_search_modified_all_per_price(model, X_candidate_BO, bounds_norm, q=BATCH_SIZE, LIGANDS_candidate_BO=LIGANDS_candidate_BO, price_dict_BO=price_dict_BO)
     indices = index_set_rearranged[0]
     candidates = candidates_rearranged[0]
-    #convert to torch tensor
+    #convert to back torch tensor
     candidates = torch.from_numpy(candidates).float()
 
 
-    # Assuming gibbon_search, update_X_y, compute_price_acquisition_ligands, check_better, update_model, and update_price_dict_ligands are defined elsewhere
-    #indices, candidates = gibbon_search(model, X_candidate_BO, bounds_norm, q=BATCH_SIZE)
     X, y = update_X_y(X, y, candidates, y_candidate_BO, indices)
     NEW_LIGANDS = LIGANDS_candidate_BO[indices]
     suggested_costs_all, _ = compute_price_acquisition_ligands(

utils.py
@@ -868,15 +868,22 @@ def compute_price_acquisition_ligands_price_per_acqfct(NEW_LIGANDS, price_dict):
 
     check_dict = cp.deepcopy(price_dict)
     test_this = np.array(list(check_dict.values()))
+    
+    max_value = max(test_this)
 
+    #divide all values by the smallest nonzero value
+    for key in check_dict:
+        if check_dict[key] != 0:
+            check_dict[key] = check_dict[key]/max_value
+    min_value = find_smallest_nonzero(test_this)
     #pdb.set_trace()
     price_per_ligand = []
     for ligand in NEW_LIGANDS:
         #test_this = np.array(list(check_dict.values()))
         #min_value = find_smallest_nonzero(test_this)
-        price_per_ligand.append(1+np.log(check_dict[ligand]))
+        price_per_ligand.append( min_value+  np.log(check_dict[ligand]))
         #price_per_ligand.append(1+check_dict[ligand]-min_value)
-        check_dict[ligand] = 1
+        check_dict[ligand] = min_value
 
     price_per_ligand = np.array(price_per_ligand)
 