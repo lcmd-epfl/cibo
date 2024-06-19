997f79f9e5c37fdbdcd8f13c1bade5c48a78a722
janweinreich <janweinreich286@gmail.com>
2024-06-19 10:26:14 +0200

    ...

--- cibo/data/user_data.py
+++ cibo/data/user_data.py
@@ -1,99 +1,10 @@
 import numpy as np
 import pandas as pd
 from cibo.utils import FingerprintGenerator
-import pdb
+
 
 
 class user_data:
-    def __init__(self, csv_file="user_data.csv"):
-        # direct arylation reaction
-        self.ECFP_size = 512
-        self.radius = 2
-        self.ftzr = FingerprintGenerator(nBits=self.ECFP_size, radius=self.radius)
-
-        self.data = pd.read_csv(csv_file)
-        self.data = self.data.sample(frac=1).reset_index(drop=True)
-        # create a copy of the data
-        data_copy = self.data.copy()
-        # remove the Yield column from the copy
-        data_copy.drop("Yield", axis=1, inplace=True)
-        # check for duplicates
-        duplicates = data_copy.duplicated().any()
-        if duplicates:
-            print("There are duplicates in the dataset.")
-            exit()
-
-        col_0_base = self.ftzr.featurize(self.data["Base_SMILES"])
-        col_1_ligand = self.ftzr.featurize(self.data["Ligand_SMILES"])
-        col_2_solvent = self.ftzr.featurize(self.data["Solvent_SMILES"])
-        col_3_concentration = self.data["Concentration"].to_numpy().reshape(-1, 1)
-        col_4_temperature = self.data["Temp_C"].to_numpy().reshape(-1, 1)
-        self.X = np.concatenate(
-            [
-                col_0_base,
-                col_1_ligand,
-                col_2_solvent,
-                col_3_concentration,
-                col_4_temperature,
-            ],
-            axis=1,
-        )
-        self.experiments = np.concatenate(
-            [
-                self.data["Base_SMILES"].to_numpy().reshape(-1, 1),
-                self.data["Ligand_SMILES"].to_numpy().reshape(-1, 1),
-                self.data["Solvent_SMILES"].to_numpy().reshape(-1, 1),
-                self.data["Concentration"].to_numpy().reshape(-1, 1),
-                self.data["Temp_C"].to_numpy().reshape(-1, 1),
-                self.data["Yield"].to_numpy().reshape(-1, 1),
-            ],
-            axis=1,
-        )
-
-        self.y = self.data["Yield"].to_numpy()
-        self.all_ligands = self.data["Ligand_SMILES"].to_numpy()
-        self.all_bases = self.data["Base_SMILES"].to_numpy()
-        self.all_solvents = self.data["Solvent_SMILES"].to_numpy()
-        unique_bases = np.unique(self.data["Base_SMILES"])
-        unique_ligands = np.unique(self.data["Ligand_SMILES"])
-        unique_solvents = np.unique(self.data["Solvent_SMILES"])
-        unique_concentrations = np.unique(self.data["Concentration"])
-        unique_temperatures = np.unique(self.data["Temp_C"])
-
-        max_yield_per_ligand = np.array(
-            [
-                max(self.data[self.data["Ligand_SMILES"] == unique_ligand]["Yield"])
-                for unique_ligand in unique_ligands
-            ]
-        )
-
-        self.worst_ligand = unique_ligands[np.argmin(max_yield_per_ligand)]
-        # make price of worst ligand 0 because already in the inventory
-        self.best_ligand = unique_ligands[np.argmax(max_yield_per_ligand)]
-
-        self.where_worst_ligand = np.array(
-            self.data.index[self.data["Ligand_SMILES"] == self.worst_ligand].tolist()
-        )
-
-        self.feauture_labels = {
-            "names": {
-                "bases": unique_bases,
-                "ligands": unique_ligands,
-                "solvents": unique_solvents,
-                "concentrations": unique_concentrations,
-                "temperatures": unique_temperatures,
-            },
-            "ordered_smiles": {
-                "bases": self.data["Base_SMILES"],
-                "ligands": self.data["Ligand_SMILES"],
-                "solvents": self.data["Solvent_SMILES"],
-                "concentrations": self.data["Concentration"],
-                "temperatures": self.data["Temp_C"],
-            },
-        }
-
-
-class user_data2:
 
     def __init__(
         self,
@@ -194,4 +105,4 @@ class user_data2:
 
 
 if __name__ == "__main__":
-    user_data()
+    print("Testing user_data.py")
